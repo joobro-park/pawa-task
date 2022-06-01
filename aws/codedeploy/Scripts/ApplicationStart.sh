6db03afcc205c88a6073c35e87c8214487808e06
joobro <wiseoldster@gmail.com>
2022-06-02 01:49:32 +0900

    add appspec.yml

--- aws/codedeploy/Scripts/ApplicationStart.sh
+++ aws/codedeploy/Scripts/ApplicationStart.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-#service nginx restart
+DEPLOY_LOG = /home/ubuntu/deploy.log
+JAR_NAME = $(ls /var/app/*.jar) | cut -d " " -f1
+nohup java -jar $JAR_NAME >> $DEPLOY_LOG 2>&1 &
--- aws/codedeploy/Scripts/BeforeInstall.sh
+++ aws/codedeploy/Scripts/BeforeInstall.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
 echo "Before Install"
+echo "check java exist"
+if [ -z $JAVA_HOME ]; then
+    apt update
+    apt install -y openjdk-12-jdk
+    export JAVA_HOME=/usr/lib/jvm/java-12-openjdk-amd64
+fi
+
+echo "kill old process"
+JAR_PATH = $(ls /var/app/*.jar | cut -d " " -f1)
+JAR_NAME = $(basename $JAR_NAME)
+if [ -n JAR_NAME ]; then
+    CURRENT_PID = $(pgrep -f JAR_NAME)
+    if [ -n $CURRENT_PID ]; then
+        kill -15 $CURRENT_PID
+        sleep 5
+    fi
+fi
