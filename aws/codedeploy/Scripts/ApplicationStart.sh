b03281ac81cca04dd3e43a0f05d1f9d4ebb4da1b
joobro <wiseoldster@gmail.com>
2022-06-06 19:19:33 +0900

    nohup is hard

--- aws/codedeploy/Scripts/ApplicationStart.sh
+++ aws/codedeploy/Scripts/ApplicationStart.sh
@@ -10,5 +10,6 @@ if [ ! -e $DEPLOY_LOG ]; then
 fi
 
 JAR_PATH=$(ls /var/app/*.jar | cut -d " " -f1)
-cd /
-nohup $(which java) -jar $JAR_PATH >> $DEPLOY_LOG 2>&1 &
+JAVA_BIN=$(which java)
+echo $JAVA_BIN
+nohup $JAVA_BIN -jar $JAR_PATH < /dev/null > $DEPLOY_LOG 2>$DEPLOY_LOG &
