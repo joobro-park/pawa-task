b326bb0b94a79ad7e41597f417c7b84cc38c4657
joobro <wiseoldster@gmail.com>
2022-06-06 17:54:55 +0900

    create log file if not exist

--- aws/codedeploy/Scripts/ApplicationStart.sh
+++ aws/codedeploy/Scripts/ApplicationStart.sh
@@ -2,5 +2,11 @@
 
 echo "Application Start"
 DEPLOY_LOG=/home/ubuntu/deploy.log
+if [ ! -e $DEPLOY_LOG ]; then
+  echo "create log file";
+  touch $DEPLOY_LOG;
+  chmod 666 $DEPLOY_LOG;
+fi
+
 JAR_PATH=$(ls /var/app/*.jar | cut -d " " -f1)
 nohup java -jar $JAR_PATH >> $DEPLOY_LOG 2>&1 &
