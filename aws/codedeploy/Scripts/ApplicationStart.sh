74f9580708754ce5eed8a4f764150e3e1b07684f
joobro <wiseoldster@gmail.com>
2022-06-06 17:17:42 +0900

    too hard..

--- aws/codedeploy/Scripts/ApplicationStart.sh
+++ aws/codedeploy/Scripts/ApplicationStart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-DEPLOY_LOG = /home/ubuntu/deploy.log
-JAR_NAME = $(ls /var/app/*.jar) | cut -d " " -f1
+export DEPLOY_LOG=/home/ubuntu/deploy.log
+export JAR_NAME=$(ls /var/app/*.jar | cut -d " " -f1)
 nohup java -jar $JAR_NAME >> $DEPLOY_LOG 2>&1 &
